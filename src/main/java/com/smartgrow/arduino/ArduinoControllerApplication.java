ce6453ecd1def2f36de052ec8efe52bab08bc8ac
Danilo Perassoli de Souza <danilop@ciandt.com>
2017-08-30 15:29:40 -0300

    Initial commit

---
+++ src/main/java/com/smartgrow/arduino/ArduinoControllerApplication.java
@@ -0,0 +1,12 @@
+package com.smartgrow.arduino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArduinoControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArduinoControllerApplication.class, args);
+	}
+}
