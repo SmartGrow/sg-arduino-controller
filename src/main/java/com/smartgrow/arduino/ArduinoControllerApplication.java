842819f0adc4590bd795552a0016cc91eacb1cfb
Danilo Perassoli de Souza <danilop@ciandt.com>
2017-08-31 18:56:40 -0300

    Added filter and security

--- src/main/java/com/smartgrow/arduino/ArduinoControllerApplication.java
+++ src/main/java/com/smartgrow/arduino/ArduinoControllerApplication.java
@@ -1,8 +1,12 @@
 package com.smartgrow.arduino;
 
+import com.smartgrow.springboot.web.filter.EnableSgFilters;
+import com.smartgrow.springboot.web.security.EnableSgSecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableSgFilters
+@EnableSgSecurity
 @SpringBootApplication
 public class ArduinoControllerApplication {
 
