24ec5c6afa34be38613516290bcf3c184677e4e6
Danilo Perassoli de Souza <danilop@ciandt.com>
2017-12-05 13:21:00 -0200

    Disabling repl for board

--- server.js
+++ server.js
@@ -3,22 +3,20 @@ const app = express()
 
 var pause = false;
 
-function connectToBoard() {
-    const five = require("johnny-five");
-    const board = new five.Board()
+// johnny-five stuff
+const five = require("johnny-five");
+const board = new five.Board({repl: false})
 
-    board.on("ready", function() {
-        this.pinMode(13, this.MODES.OUTPUT);
-        this.loop(500, () => {
-            if (!pause) {
-                this.digitalWrite(13, this.pins[13].value ? 0 : 1);
-            }
-        });
+board.on("ready", function() {
+    this.pinMode(13, this.MODES.OUTPUT);
+    this.loop(500, () => {
+        if (!pause) {
+            this.digitalWrite(13, this.pins[13].value ? 0 : 1);
+        }
     });
-}
+});
 
-setTimeout(connectToBoard, 5000);
-
+// Express stuff
 app.get('/', (req, res) => res.send('Hello World!'))
 
 app.get('/toggle', (req, res) => {
@@ -26,4 +24,4 @@ app.get('/toggle', (req, res) => {
     res.sendStatus(200);
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(3000, () => console.log('App listening on port 3000!'))
