805cf0b78ffd37d8c6c4d5945389914e69e029fb
Danilo Perassoli de Souza <danilop@ciandt.com>
2017-12-05 12:18:08 -0200

    Added timeout to connect to board (maybe it will prevent from closing the board after init)

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
+import { setTimeout } from 'timers';
+
 const express = require('express')
 const app = express()
 
-const five = require("johnny-five");
-const board = new five.Board()
-
 var pause = false;
 
-board.on("ready", function() {
-    
+function connectToBoard() {
+    const five = require("johnny-five");
+    const board = new five.Board()
 
-    this.pinMode(13, this.MODES.OUTPUT);
-    
-    this.loop(500, () => {
-        if (!pause) {
-            this.digitalWrite(13, this.pins[13].value ? 0 : 1);
-        }
+    board.on("ready", function() {
+        this.pinMode(13, this.MODES.OUTPUT);
+        this.loop(500, () => {
+            if (!pause) {
+                this.digitalWrite(13, this.pins[13].value ? 0 : 1);
+            }
+        });
     });
+}
 
-});
+setTimeout(connectToBoard, 5000);
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
