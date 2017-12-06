7fa5fdb6af678a6818fff3080f93599394f07740
Danilo Perassoli de Souza <danilop@ciandt.com>
2017-12-06 18:19:13 -0200

    Changed express endpoints to /api/*

--- app/app.js
+++ app/app.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 store.initialize();
 
 // Routes
-app.use('/users', users);
+app.use('/api/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
